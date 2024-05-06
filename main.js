b58d8459b4f2c6c43062bb48b6d45682b0819c4e
wizzkychick <winachika8@gmail.com>
2024-05-06 17:04:08 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// alert('hai roamer')
+
+// // prompt('berapa hero andalanmu?') //untuk menjalankan pertanyaan popup suatu sistem
+// // var hero = 5
+// // const hero = 10
+// // let hero = 5
+// // hero = 5
+// // console.log('hai hero andalan kamu ada ' + hero)
+// // alert('hero andalan kamu ada ' + hero)
+// // let hero = prompt('berapa hero andalanmu?')
+// // alert('hero andalan kamu ada ' + hero)
+
+let nama = 'isak' // tipe string
+let usia = 80 // tipe number
+let tinggiBadan = 150 // tipe double . float
+let beratBadan = null
+
+
+if(beratBadan == null) {
+    beratBadan = 'gendut kali pokoknya'}
+else {
+    beratBadan = 'besar lah pokoknya'
+}
+
+alert(`namamu ${nama} kan`,)
+
+alert(`usiamu pasti ${usia} yakan`,)
+
+alert(`berat badanmu ${beratBadan}`,)
